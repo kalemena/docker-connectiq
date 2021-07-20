12ca5fdfe64e6333df854590976c149766cd859e
kalemena <kalemena@free.fr>
2021-07-20 08:11:16 +0200

    Cleanup repo files

--- generate-certificate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /workspace
-openssl genrsa -out developer_key.pem 4096
-openssl pkcs8 -topk8 -inform PEM -outform DER -in /workspace/developer_key.pem -out /workspace/developer_key.der -nocrypt
